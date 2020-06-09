87848dc455106b7e72afabce15a7a0bd80d52f5e
Yifei Liu <37576347+luojuee@users.noreply.github.com>
2020-06-09 23:49:19 +0200

    Update Game.js

--- DVIproyecto/js/Game.js
+++ DVIproyecto/js/Game.js
@@ -362,6 +362,7 @@ export default class Game extends Phaser.Scene {
       this.physics.world.colliders.destroy();
     }
     if((this.player.getNumlife() <= 0)&&(this.player.y > 770)){
+        this.scene.destroy();
         this.scene.launch('termenu');
         this.scene.stop('playgame');
     }
