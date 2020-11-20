79cc579c7e20b23a438ed961f00c677dbd2fd946
Yifei Liu <37576347+luojuee@users.noreply.github.com>
2020-11-20 17:14:05 +0100

    1

--- estilo/js/Main.js
+++ estilo/js/Main.js
@@ -3,13 +3,19 @@ export default class Main extends Phaser.Scene {
 	    super({ key: 'main' });
 	}
 	preload() {  
-	  	this.load.image('capainferior', 'assets/fondos/layers/1.png');
+		  this.load.image('capainferior', 'assets/fondos/layers/1.png');
+		  this.load.image('segundacapa', 'assets/fondos/layers/2.png');
+		  this.load.image('primeracapa', 'assets/fondos/layers/3.png');
 	}
 	create() {
 		  this.capainfe = this.add.tileSprite(0, 0, this.game.config.width*2, this.game.config.height*2, 'capainferior');
+		  this.capa2 = this.add.tileSprite(0, 0, this.game.config.width*2, this.game.config.height*2, 'segundacapa');
+		  this.capa1 = this.add.tileSprite(0, 0, this.game.config.width*2, this.game.config.height*2, 'primeracapa');
 	}
 
 	update(titleme, delta) {
-	    this.capainfe.tilePositionX += 0.2
+		this.capa1.tilePositionX += 0.6
+		this.capa2.tilePositionX += 0.4
+		this.capainfe.tilePositionX += 0.2
 	}
 }
